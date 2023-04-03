87e3d240bee052159470d12ce2b49088658a9daf
Mik-Halo <sprone3@gmail.com>
2023-04-03 11:43:44 +0300

    minor styles correction

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,7 +3,7 @@ import "./Loading.css";
 
 export default function Loading() {
   return (
-    <svg className="position-absolute top-50 start-50 translate-middle" viewBox="25 25 50 50">
+    <svg className="position-absolute top-50 start-50 translate-middle preloader" viewBox="25 25 50 50">
       <circle r="20" cy="50" cx="50"></circle>
     </svg>
   )
